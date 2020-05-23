4d4e7471f9e1fdbe886f655b2c9da7f81bd9c2e6
razdvapoka <razdvapoka@gmail.com>
2020-05-23 15:11:37 +0300

    :sparkles: Add sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,12 +17,16 @@ const IndexPage = ({ data: { contentfulPage: pageData } }) => {
   return (
     <Layout toggleMenu={toggleMenu} isMenuOpen={isMenuOpen} headerText={pageData.headerText}>
       <SEO title="художественный" />
+      <Section titleId="gallery"></Section>
       <Section titleId="history">
         <div className="flex">
           <div className="w-1/2"></div>
           <Markdown className="text-ml w-1/2">{pageData.history.history}</Markdown>
         </div>
       </Section>
+      <Section titleId="team"></Section>
+      <Section titleId="contacts"></Section>
+      <footer>footer</footer>
     </Layout>
   )
 }
