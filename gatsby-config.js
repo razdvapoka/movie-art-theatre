1f82a07678753f4d03873842e86f1e4a1ada0a8b
razdvapoka <razdvapoka@gmail.com>
2020-05-24 21:37:47 +0300

    :wrench: Whitelist a few classes

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,11 +47,17 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        whitelist: [], // Don't remove this selector
-        ignore: [
-          "/node_modules/defaults.css/defaults.css",
-          "src/components/markdown/index.module.styl",
-        ], // Ignore files/folders
+        whitelist: [
+          "item-enter",
+          "item-enter-active",
+          "item-exit",
+          "item-exit-active",
+          "fade-enter",
+          "fade-enter-active",
+          "fade-exit",
+          "fade-exit-active",
+        ], // Don't remove this selector
+        ignore: ["src/components/markdown/index.module.styl"], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
